2058c63a3a901507aaf49b18bf5eaf5c17e9411c
saber011 <shevchu2012@mail.ru>
2020-04-14 22:48:37 +0500

    temp

--- JWT/ViewModel/UserView.cs
+++ JWT/ViewModel/UserView.cs
@@ -7,7 +7,6 @@ namespace JWT.ViewModel
 {
     public class UserView
     {
-        public int Id { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
 
