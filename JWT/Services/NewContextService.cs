2159da9f5abdeec17776fc33beb96413860d8fdc
saber011 <shevchu2012@mail.ru>
2020-05-15 23:00:04 +0500

    Temp

---
+++ JWT/Services/NewContextService.cs
@@ -0,0 +1,21 @@
+﻿using JWT.Interfaces;
+using JWT.Models;
+using JWT.NewApplogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWT.Services
+{
+    public class NewContextService : INewContextService
+    {
+        private readonly ApplicationContext _context;
+        private Graph gragp { get; set; }
+
+        public NewContextService(ApplicationContext _applicationContext) =>
+           _context = _applicationContext;
+
+
+    }
+}
